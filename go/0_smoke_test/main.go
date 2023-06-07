ae3c5a629119d82f0889635f2a3149ea3f8cb8e4
ishwar00 <ishwargowda100@gmail.com>
2023-06-08 02:27:38 +0530

    refactor: create `handleConn` function

--- go/0_smoke_test/main.go
+++ go/0_smoke_test/main.go
@@ -12,25 +12,26 @@ import (
 func main() {
 	server := server.New("tcp", "5001")
 	fmt.Println("tcp server listening on port 5001...")
-
-	handleConn := func(conn net.Conn) {
-		defer conn.Close()
-		fmt.Println("received new connection!!")
-
-		buff := make([]byte, 1024)
-		for {
-			n, err := conn.Read(buff)
-			if err != nil {
-				if err != io.EOF {
-					log.Fatal(err)
-				}
-				return
-			}
-			fmt.Println(string(buff[:n]))
-			conn.Write(buff[:n])
-		}
-	}
-
 	server.Start(handleConn)
+}
+
+func handleConn(conn net.Conn) {
+	defer func() {
+		conn.Close()
+	}()
+
+	fmt.Println("received new connection!!")
+
+	buff := make([]byte, 1024)
+	for {
+		n, err := conn.Read(buff)
+		if err != nil {
+			if err != io.EOF {
+				log.Println(err)
+			}
+			return
+		}
+		conn.Write(buff[:n])
+	}
 }
 
